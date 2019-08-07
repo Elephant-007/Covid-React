numba/numba

commit 52caaf2ccd78aee4995fe3e1aca16af271b9d944

Attempt delayed removal of Env

numba/lowering.py
@@ -1,7 +1,8 @@
 from __future__ import print_function, division, absolute_import
 
 import weakref
-from collections import namedtuple
+import time
+from collections import namedtuple, deque
 from functools import partial
 
 from llvmlite.llvmpy.core import Constant, Type, Builder
@@ -12,7 +13,7 @@ from . import (_dynfunc, cgutils, config, funcdesc, generators, ir, types,
 from .errors import LoweringError, new_error_context
 from .targets import removerefctpass
 from .funcdesc import default_mangler
-from . import debuginfo
+from . import debuginfo, utils
 
 
 class Environment(_dynfunc.Environment):
@@ -37,13 +38,26 @@ class Environment(_dynfunc.Environment):
             cls._memo[fndesc.env_name] = inst
             return inst
 
-
     def __reduce__(self):
         return _rebuild_env, (
             self.globals['__name__'],
             self.consts,
             self.env_name
             )
+
+    def __del__(self):
+        if utils.IS_PY3:
+            return
+        if _keepalive is None:
+            return
+        _keepalive.append((time.time(), self))
+        if len(_keepalive) > 10:
+            cur = time.time()
+            while _keepalive and cur - _keepalive[0][0] > 1:
+                _keepalive.popleft()
+
+
+_keepalive = deque()
 
 
 def _rebuild_env(modname, consts, env_name):

numba/tests/test_func_lifetime.py
@@ -124,6 +124,7 @@ class TestFuncLifetime(TestCase):
         gc.collect()
         self.assertEqual([w() for w in wrs], [None] * len(wrs))
 
+    @unittest.skipUnless(IS_PY3, "py3 only; known leak in py2")
     def test_inner_function_lifetime(self):
         self.check_inner_function_lifetime(forceobj=True)
 

Elephant-007/Covid-React

commit 0482fda8bb2d2f949e719b9a9b08390be9db7c2e

Fixed two module teardown races in py2.
Commit 52caaf2ccd78aee4995fe3e1aca16af271b9d944 introduced two
single-threaded race conditions in Python 2 between the module
teardown mechanism and Environment.__del__.

 - If the numba.lowering module is torn down too early, its `time`
   global variable (referencing the time module) can be reset to
   None. The commit avoids a similar race with the _keepalive global
   variable, but that extra None-checking logic is missing for the
   time module. In practice, this race is almost never lost.

 - If the time module is torn down before the last Environment.__del__
   method is called, but before numba.lowering is torn down then
   time.time will be None. In practice, this is the race that is
   frequently lost when certain complex applications exit. In such
   situations, the following message is printed to stderr:

      Exception TypeError: "'NoneType' object is not callable" in
      <bound method Environment.__del__ of <numba.lowering.Environment
      object at ...>> ignored

This commit fixes both races.

numba/lowering.py
@@ -50,6 +50,8 @@ class Environment(_dynfunc.Environment):
             return
         if _keepalive is None:
             return
+        if time is None or time.time is None:
+            return
         _keepalive.append((time.time(), self))
         if len(_keepalive) > 10:
             cur = time.time()